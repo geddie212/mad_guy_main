361176826fbdeb87a5b98cd6f80340e5c8584d81
geddie212 <pauliusgedrimas@gmail.com>
2023-06-12 19:00:20 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# Mad Guy Website using Flask Server Framework
+# Made by Paul Ged - 12 June 2023
+import datetime
+from flask import Flask, render_template
+
+# creates app to run
+app = Flask(__name__)
+
+# get current year for website footer
+now_time = datetime.datetime.now()
+year = now_time.year
+
+
+@app.route("/")
+def index():
+    # renders index.html file and adds year variable for jinja
+    return render_template("index.html", year=year)
+
+
+# runs app from main.py
+if __name__ == "__main__":
+    app.run()
